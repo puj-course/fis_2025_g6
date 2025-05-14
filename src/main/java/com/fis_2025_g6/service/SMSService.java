70b2cdd722b6986d380c8c972c346da2d71aa120
AndreMD287 <bastianlanfor@gmail.com>
2025-05-14 02:29:22 -0500

    Recuperando archivos perdidos desde el stash

---
+++ src/main/java/com/fis_2025_g6/service/SMSService.java
@@ -0,0 +1,31 @@
+package com.fis_2025_g6.service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+public class SMSService {
+
+    //credenciales
+    public static final String ACCOUNT_SID = "SID";
+    public static final String AUTH_TOKEN = "TOKEN";
+    public static final String FROM_PHONE_NUMBER = "+18573763394"; // Número de Twilio
+
+    static {
+        // Inicializa la conexión con Twilio
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    }
+    public static void enviarSMS(String numeroDestino, String mensaje) {
+        try {
+            Message sms = Message.creator(
+                    new PhoneNumber(numeroDestino),
+                    new PhoneNumber(FROM_PHONE_NUMBER),
+                    mensaje
+            ).create();
+
+            System.out.println("SMS enviado correctamente. SID: " + sms.getSid());
+        } catch (Exception e) {
+            System.out.println("Error al enviar SMS: " + e.getMessage());
+        }
+    }
+}
