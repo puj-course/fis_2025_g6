2911c86501498a8435095a9b11edd6c40d0b7178
Eliqn <69222564+Eliqn@users.noreply.github.com>
2025-05-15 19:28:23 -0500

    :memo: feat(api): agregar documentación de la API #73

--- src/main/java/com/fis_2025_g6/config/SecurityConfig.java
+++ src/main/java/com/fis_2025_g6/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
